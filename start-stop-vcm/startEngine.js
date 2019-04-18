fa659e2f3d50867d6ede35ed07fbf0f0b642b641
KareemG <kareem@icekode.com>
2019-04-17 22:47:15 -0400

    Updated the base code and the framework going forward. • 4.17.19 - Created the base functions for the start and stop code and added them to the server.  Next step is to update the instance information and get it set up for a test run. The need to make the code dynamic where you can suggest the instance and zone through json of what you want to shut down.

--- start-stop-vcm/startEngine.js
+++ start-stop-vcm/startEngine.js
@@ -1,11 +1,14 @@
 var http = require('http');
 var Compute = require('@google-cloud/compute');
-var compute = Compute();
+var compute = new Compute();
+
 exports.startInstance = function startInstance(req, res) {
     var zone = compute.zone('instance zone here');
     var vm = zone.vm('instance name here');
+
     vm.start(function(err, operation, apiResponse) {
         console.log('instance start successfully');
     });
-res.status(200).send('Success start instance');
+
+    res.status(200).send('Success start instance');
 };
--- start-stop-vcm/stopEngine.js
+++ start-stop-vcm/stopEngine.js
@@ -1,8 +1,11 @@
 var Compute = require('@google-cloud/compute');
-var compute = Compute();
+var compute = new Compute();
+
 exports.stopInstance = function stopInstance(req, res) {
     var zone = compute.zone('instance zone here');
     var vm = zone.vm('instance name here');
+
+
     vm.stop(function(err, operation, apiResponse) {
         console.log('instance stop successfully');
     });
