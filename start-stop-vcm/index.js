2de1f5dd295897df5f1bbd6000accbb3d2d5f2dd
KareemG <kareem@icekode.com>
2019-04-20 09:51:50 -0400

    Made structural changes to align with Google Cloud Function format https://www.pivotaltracker.com/story/show/165257478

--- start-stop-vcm/index.js
+++ start-stop-vcm/index.js
@@ -17,3 +17,19 @@ exports.stopInstance = function stopInstance(req, res) {
     });
     res.status(200).send('Success stop instance');
 };
+
+/**
+ * Google computer function that handles the starting of a VM instance
+ * @param req
+ * @param res
+ */
+exports.startInstance = function startInstance(req, res) {
+    var zone = compute.zone(instanceZone);
+    var vm = zone.vm(instanceName);
+
+    vm.start(function(err, operation, apiResponse) {
+        console.log('instance start successfully');
+    });
+
+    res.status(200).send('Success start instance');
+};
