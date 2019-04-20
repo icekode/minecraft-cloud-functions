bbb7027dcec6a1a4b450635d2d2577311fd6b460
KareemG <kareem@icekode.com>
2019-04-20 09:25:09 -0400

    Updated start and start script to be more dynamic in the future. Added additional comments. https://www.pivotaltracker.com/story/show/165257478

--- start-stop-vcm/startEngine.js
+++ start-stop-vcm/startEngine.js
@@ -1,10 +1,17 @@
 var http = require('http');
 var Compute = require('@google-cloud/compute');
 var compute = new Compute();
+var instanceZone = 'us-east4-a'; //TODO Build instanceZone into RES String
+var instanceName = 'mcp-server'; //TODO build instanceName into RES String
 
+/**
+ * Google computer function that handles the starting of a VM instance
+ * @param req
+ * @param res
+ */
 exports.startInstance = function startInstance(req, res) {
-    var zone = compute.zone('instance zone here');
-    var vm = zone.vm('instance name here');
+    var zone = compute.zone(instanceZone);
+    var vm = zone.vm(instanceName);
 
     vm.start(function(err, operation, apiResponse) {
         console.log('instance start successfully');
--- start-stop-vcm/stopEngine.js
+++ start-stop-vcm/stopEngine.js
@@ -1,10 +1,16 @@
 var Compute = require('@google-cloud/compute');
 var compute = new Compute();
+var instanceZone = 'us-east4-a'; //TODO Build instanceZone into RES String
+var instanceName = 'mcp-server'; //TODO build instanceName into RES String
 
+/**
+ * Google compute function that handles the stopping of a VM instance
+ * @param req
+ * @param res
+ */
 exports.stopInstance = function stopInstance(req, res) {
-    var zone = compute.zone('instance zone here');
-    var vm = zone.vm('instance name here');
-
+    var zone = compute.zone(instanceZone);
+    var vm = zone.vm(instanceName);
 
     vm.stop(function(err, operation, apiResponse) {
         console.log('instance stop successfully');
